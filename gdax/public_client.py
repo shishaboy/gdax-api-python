25aada81562727b41f6f75416586d964a4077bf3
shishaboy <37517181+shishaboy@users.noreply.github.com>
2018-07-23 20:06:10 -0400

    sandbox

--- gdax/public_client.py
+++ gdax/public_client.py
@@ -6,6 +6,9 @@ class PublicClient(object):
     
     The Market Data API is an unauthenticated set of endpoints for retrieving
     market data. These endpoints provide snapshots of market data.
+    
+        #https://public.sandbox.pro.coinbase.com
+    #https://api.pro.coinbase.com
     """
 
     # List of products offered as of 1/31/2018.
@@ -23,7 +26,7 @@ class PublicClient(object):
     LTC_EUR = "LTC-EUR"
     LTC_USD = "LTC-USD"
 
-    def __init__(self, api_url='https://api.pro.coinbase.com', timeout=30):
+    def __init__(self, api_url='https://public.sandbox.pro.coinbase.com', timeout=30):
         """Create GDAX API public client.
         Args:
             api_url (Optional[str]): API URL.
