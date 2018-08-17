b91c3f07e4dd1df23f36e50f71bbbc83c0a3b11f
shishaboy <37517181+shishaboy@users.noreply.github.com>
2018-08-17 14:39:42 -0400

    from sandbox to coinbase pro

--- gdax/private_client.py
+++ gdax/private_client.py
@@ -15,7 +15,7 @@ class PrivateClient(PublicClient):
     """
 
     def __init__(self, key, b64secret, passphrase,
-                 api_url="https://public.sandbox.pro.coinbase.com", timeout=30): 
+                 api_url="https://api.pro.coinbase.com", timeout=30): 
         self.url = api_url.rstrip('/')
         self.timeout = timeout
         self.auth = CoinbaseExchangeAuth(key, b64secret, passphrase)
